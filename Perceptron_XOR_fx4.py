f74099f25beb55337f309cae5716c0c73e0fdb16
Thanasin Bunnam <thanasin.b@gmail.com>
2022-01-19 09:27:03 +0700

    [WIP] Export variables to xlsx

--- Perceptron_XOR_fx4.py
+++ Perceptron_XOR_fx4.py
@@ -8,6 +8,7 @@ Created on Tue Aug 10 12:59:19 2021
 import numpy as np
 import time
 from fxpmath import Fxp as fxp
+import pandas as pd
 
 # import matplotlib.pyplot as plt
 
@@ -65,8 +66,8 @@ number_input = set_input.shape[0]
 number_answer = set_answer.shape[0]
 
 ## SET NN PARAMETERS
-nn = NeuralNetwork(2, 9, 1)
-learning_rate = 1
+nn = NeuralNetwork(2, 10, 1)
+learning_rate = 5
 epoch = 50000
 count_correct = 0
 count_error = 0
@@ -88,12 +89,18 @@ for i in range(epoch):
     y_answer = set_answer[i % number_input]
 
     ## XOR CALCULATE ACCURACY
-    num_y_guess = y_guess()
-    num_y_answer = y_answer()
+    num_y_guess = y_guess[0][0]
+    num_y_answer = y_answer()[0][0]
+
     if num_y_guess > Logic_half:
-        num_y_guess = Logic_1()
+        num_y_guess = 1
     else:
-        num_y_guess = Logic_0()
+        num_y_guess = 0
+
+    if num_y_answer > Logic_half:
+        num_y_answer = 1
+    else:
+        num_y_answer = 0
 
     if num_y_answer == num_y_guess:
         count_correct += 1
@@ -110,6 +117,27 @@ for i in range(epoch):
         elif A > Logic_half and B > Logic_half:
             count_error_11 += 1
 
+        num_y_guess = np.matrix(num_y_guess)
+        num_y_answer = np.matrix(num_y_answer)
+        with open("xor.csv", "ab") as f:
+            f.write(b"Hidden weights\n")
+            np.savetxt(f, nn.hidden_weights, delimiter=",")
+            f.write(b"Input vector\n")
+            np.savetxt(f, input, delimiter=",")
+            f.write(b"Output weights\n")
+            np.savetxt(f, nn.output_weights, delimiter=",")
+            f.write(b"Hidden vector\n")
+            np.savetxt(f, result_hidden, delimiter=",")
+            f.write(b"Y guess\n")
+            np.savetxt(f, y_guess, delimiter=",")
+            f.write(b"Y answer\n")
+            np.savetxt(f, y_answer, delimiter=",")
+            f.write(b"Y guess logic\n")
+            np.savetxt(f, num_y_guess, delimiter=",")
+            f.write(b"Y answer logic\n")
+            np.savetxt(f, num_y_answer, delimiter=",")
+            f.write(b"\n")
+
         ## BACKPROPAGATION
         output_error = y_answer() - y_guess()
         output_delta = np.dot(learning_rate * np.multiply(dSigmoid(y_guess()), output_error), result_hidden().T)
@@ -121,6 +149,25 @@ for i in range(epoch):
         hidden_weights_real = np.add(nn.hidden_weights(), hidden_delta)
         nn.output_weights = fxp(output_weights_real, signed=True, n_word=word, n_frac=frac, rounding='around')
         nn.hidden_weights = fxp(hidden_weights_real, signed=True, n_word=word, n_frac=frac, rounding='around')
+
+        with open("xor.csv", "ab") as f:
+            f.write(b"Output error\n")
+            np.savetxt(f, output_error, delimiter=",")
+            f.write(b"Output delta\n")
+            np.savetxt(f, output_delta, delimiter=",")
+            f.write(b"Hidden error\n")
+            np.savetxt(f, hidden_error, delimiter=",")
+            f.write(b"Hidden delta\n")
+            np.savetxt(f, hidden_delta, delimiter=",")
+            f.write(b"New hidden weights real\n")
+            np.savetxt(f, hidden_weights_real, delimiter=",")
+            f.write(b"New output weights real\n")
+            np.savetxt(f, output_weights_real, delimiter=",")
+            f.write(b"New hidden weights fix\n")
+            np.savetxt(f, nn.hidden_weights, delimiter=",")
+            f.write(b"New output weights fix\n")
+            np.savetxt(f, nn.output_weights, delimiter=",")
+            f.write(b"\n")
 
     if (i % round_print == 0):
         print('epoch: ', i, 'accuracy: ', round((count_correct / round_print) * 100.0, 2), '%')
